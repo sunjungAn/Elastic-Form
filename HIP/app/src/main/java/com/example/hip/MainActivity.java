fed653e6496facf0c1dd8c81faa0c22817496a3c
sunjungAn <dkstjswjd83@naver.com>
2021-05-20 15:31:10 +0900

    지도 오류:

--- HIP/app/src/main/java/com/example/hip/MainActivity.java
+++ HIP/app/src/main/java/com/example/hip/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.hip;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.pm.PackageInfo;
@@ -9,23 +10,29 @@ import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
 import android.view.ViewGroup;
-
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import net.daum.mf.map.api.MapView;
 
 import java.security.MessageDigest;
 
 public class MainActivity extends AppCompatActivity {
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_map);
 
-        MapView mapView = new MapView(this);
+        //MapView mapView = new MapView(MainActivity.this);
+
+        //RelativeLayout mapViewContainer = (RelativeLayout) findViewById(R.id.map_view);
+        //mapViewContainer.addView(mapView);
 
 
-        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
-        mapViewContainer.addView(mapView);
+
 
     }
+
 
 }
