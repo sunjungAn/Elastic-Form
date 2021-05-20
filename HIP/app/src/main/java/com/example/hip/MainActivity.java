ce1beddfe8795bdfc6fa8b35e54a376a83f0696e
sunjungAn <dkstjswjd83@naver.com>
2021-05-20 12:37:22 +0900

    map 적용중 오류

--- HIP/app/src/main/java/com/example/hip/MainActivity.java
+++ HIP/app/src/main/java/com/example/hip/MainActivity.java
@@ -8,34 +8,24 @@ import android.content.pm.Signature;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
+import android.view.ViewGroup;
+
+import net.daum.mf.map.api.MapView;
 
 import java.security.MessageDigest;
 
 public class MainActivity extends AppCompatActivity {
-
-    private void getAppKeyHash() {
-        try {
-            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
-            for (Signature signature : info.signatures) {
-                MessageDigest md;
-                md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                String something = new String(Base64.encode(md.digest(), 0));
-                Log.e("Hash key", something);
-            }
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            Log.e("name not found", e.toString());
-        }
-    }
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        getAppKeyHash();
+        setContentView(R.layout.main_map);
+
+        MapView mapView = new MapView(this);
+
+
+        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
+
     }
 
 }
