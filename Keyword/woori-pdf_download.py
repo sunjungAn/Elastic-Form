2e07eca7a17695658844bb4b26f728e0314e99d2
hyunmin0317 <choihm9903@naver.com>
2021-07-07 00:45:38 +0900

    pdf-download

--- Keyword/woori-pdf_download.py
+++ Keyword/woori-pdf_download.py
@@ -1,4 +1,46 @@
+import csv
+import pandas as pd
+import matplotlib.pyplot as plt
+import urllib.request
+import sys
+import os
+from bs4 import BeautifulSoup
+from urllib.parse import quote
+from urllib.request import Request, urlopen
 from urllib import request
+
+WOORI_URL = 'http://www.wfri.re.kr'
+TARGET_URL = '/home/sub01_01.php?stxt3=&sse=&skind=TOTAL&sword='
+download_links = []
+
+# 기사 검색 페이지에서 기사 제목에 링크된 기사 본문 주소 받아오기
+def get_link_from_news_title(URL):
+    
+        source_code_from_URL = urllib.request.urlopen(URL)
+        soup = BeautifulSoup(source_code_from_URL, 'lxml', from_encoding='utf-8')
+
+        for title in soup.find_all('td', {'class':'t_left tit'}):
+            title_link = title.select('a')
+            article_URL = WOORI_URL + title_link[0]['href']
+            get_download_link(article_URL)
+
+
+def get_download_link(URL):
+    source_code_from_URL = Request(URL, headers={'User-Agent': 'Mozilla/5.0'})
+    soup = BeautifulSoup(urlopen(source_code_from_URL), 'lxml', from_encoding='utf-8')
+    for item in soup.find_all('div', {'class':'disk'}):
+        link = item.select('a')
+        download_link = WOORI_URL + link[0]['href']
+    download_links.append(download_link)
+
+    
+def createFolder(directory):
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    except OSError:
+        print ('Error: Creating directory. ' +  directory)
+
 
 def download(url, fname):
     mem = request.urlopen(url).read()
@@ -6,8 +48,15 @@ def download(url, fname):
         f.write(mem)
         print("저장되었습니다.")
 
+
 if __name__ == '__main__':
-    site = 'http://www.wfri.re.kr'
-    src = '/inc/down.php?dir=BOARD&file_name=202106_162443873696514_2.pdf&rename=(20210624)%20Industry%20Watch%202021-06%20%EA%B5%AD%EB%82%B4%20%EA%B7%B8%EB%A6%B0%EC%8A%A4%ED%83%81(Green%20Stock)%20%EC%84%B1%EA%B3%BC%EC%99%80%20%EC%8B%9C%EC%82%AC%EC%A0%90.pdf' 
-    fname = 'text.pdf'
-    download(site+src, fname)
+    keyword = '코로나'
+    path = './'+keyword + '/'
+    i = 0
+    url = WOORI_URL + TARGET_URL + quote(keyword)
+    get_link_from_news_title(url)
+    createFolder(path)
+
+    for link in download_links:
+        i += 1
+        download(link[:83], path+keyword+str(i)+'.pdf')
