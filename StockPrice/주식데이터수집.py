766ff2ad5a5ddcbb81b8805ef4b135945247cbbf
EunGi Bae <withjust7@naver.com>
2021-07-08 14:20:36 +0900

    Update 주식데이터수집.py

--- StockPrice/주식데이터수집.py
+++ StockPrice/주식데이터수집.py
@@ -3,16 +3,22 @@ import pandas as pd
 import xmltodict
 import json
 
-stockCode = '215000'
-count = '250'
-url = f'https://fchart.stock.naver.com/sise.nhn?symbol={stockCode}&timeframe=day&count={count}&requestType=0'
+b = ['081660','025980','025980','025980','025980','025980','025980','025980','025980','025980','025980']
+count = '2500'
 
-rs = requests.get(url)
-dt = xmltodict.parse(rs.text)
-js = json.dumps(dt, indent = 4)
-js = json.loads(js)
 
-data = pd.json_normalize(js['protocol']['chartdata']['item'])
-df = data['@data'].str.split('|',expand = True)
-df.columns = ['date','open','high','low','close','Volume']
-df.to_csv(stockCode)
+def a(stockCode, count):
+    url = f'https://fchart.stock.naver.com/sise.nhn?symbol={stockCode}&timeframe=day&count={count}&requestType=0'
+
+    rs = requests.get(url)
+    dt = xmltodict.parse(rs.text)
+    js = json.dumps(dt, indent=4)
+    js = json.loads(js)
+
+    data = pd.json_normalize(js['protocol']['chartdata']['item'])
+    df = data['@data'].str.split('|', expand=True)
+    df.columns = ['date', 'open', 'high', 'low', 'close', 'Volume']
+    df.to_csv('{}.csv'.format(stockCode))
+
+for i in b:
+    a(i, 2500)
